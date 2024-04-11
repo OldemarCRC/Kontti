a0f0313b713594c968f042337dc348458463ece6
Jose Oldemar Chaves Urbina <oldemar.chaves@gmail.com>
2024-04-11 18:55:42 +0300

    deleting the customer logo

--- client/src/components/logos/LogoCustomer.js
+++ client/src/components/logos/LogoCustomer.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./logo_customer.css";
 import { Link } from "react-router-dom";
 
-const logoImage = "https://www.grupodelsol.cr/Imagenes/logos/Decaprom_logosinfondo.png";
+/* const logoImage = "https://www.grupodelsol.cr/Imagenes/logos/Decaprom_logosinfondo.png"; */
 function LogoCustomer() {
   return (
     <>
       <Link to="/home">
-        <img src={logoImage} alt="Decaprom" className="navbar-logo" />
+        <h3>Your logo here</h3>
+        {/* <img src={logoImage} alt="Decaprom" className="navbar-logo" /> */}
       </Link>
     </>
   );
