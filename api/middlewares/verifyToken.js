5c348024ce9d24794d27c6a8e671b63f06217aad
Jose Oldemar Chaves Urbina <oldemar.chaves@gmail.com>
2025-03-21 11:23:21 +0200

    Few changes in verifyToken

--- api/middlewares/verifyToken.js
+++ api/middlewares/verifyToken.js
@@ -4,23 +4,31 @@ import { createError } from "../services/errorHandler.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(createError(403, "Token not provide"));
+    return next(createError(403, "Token not provided"));
   }
-  
+
   try {
     const decoded = jwt.verify(token, process.env.JWT);
     req.userId = decoded.id;
-    
+
     req.user = {
       id: decoded.id,
-      role: decoded.role, 
+      role: decoded.role,
     };
-    
+
+    // Validar el campo `role` si es necesario
+    if (!["admin", "manager", "dispatcher", "operator", "externalUser", "gate", "surveyor", "demo"].includes(decoded.role)) {
+      return next(createError(403, "Invalid role in token"));
+    }
+
     next();
   } catch (err) {
     return next(createError(401, "Unauthorized"));
   }
 };
+
+
+
 export const verifyUser = (req, res, next) => {
   verifyToken(req, res, next, () => {
     if (req.user.id === req.params.id) {
